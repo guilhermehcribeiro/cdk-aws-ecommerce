0e54e305fdb9dc5307edc70a581cc6a8cc3bb25f
Guilherme Ribeiro <g.ribeiro@ligadaweb.com.br>
2024-08-19 10:25:18 -0300

    ses: send orders email

--- lambda/orders/orderEmailsFunction.ts
+++ lambda/orders/orderEmailsFunction.ts
@@ -1,14 +1,48 @@
-import { Context, SQSEvent } from "aws-lambda";
+import { Context, SNSMessage, SQSEvent } from "aws-lambda";
+import { AWSError, SES } from "aws-sdk";
 import * as AWSXRay from "aws-xray-sdk";
+import { Envelop, OrderEvent } from "/opt/nodejs/orderEventsLayer";
+import { PromiseResult } from "aws-sdk/lib/request";
 
 AWSXRay.captureAWS(require("aws-sdk"));
-
+const sesClient = new SES();
 export async function handler(event: SQSEvent, context: Context) {
+  const emailPromises: Promise<
+    PromiseResult<SES.SendEmailResponse, AWSError>
+  >[] = [];
   event.Records.forEach((record) => {
-    console.log(record);
-    const body = JSON.parse(record.body);
-    console.log(body);
+    const body = JSON.parse(record.body) as SNSMessage;
+    emailPromises.push(sendOrderEmail(body));
   });
 
+  await Promise.all(emailPromises);
+
   return;
+}
+
+function sendOrderEmail(body: SNSMessage) {
+  const envelop = JSON.parse(body.Message) as Envelop;
+  const event = JSON.parse(envelop.data) as OrderEvent;
+
+  const params: SES.SendEmailRequest = {
+    Destination: {
+      ToAddresses: [event.email],
+    },
+    Message: {
+      Body: {
+        Text: {
+          Charset: "UTF-8",
+          Data: `Recebemos o seu pedido de nº ${event.orderId}, no valor de R$ ${event.billing.totalPrice}`,
+        },
+      },
+      Subject: {
+        Charset: "UTF-8",
+        Data: "Recebemos o seu pedido!",
+      },
+    },
+    Source: "guilhermehcr97@gmail.com",
+    ReplyToAddresses: ["guilhermehcr97@gmail.com"],
+  };
+
+  return sesClient.sendEmail(params).promise();
 }
--- lib/eventsDdb-stacks.ts
+++ lib/eventsDdb-stacks.ts
@@ -24,11 +24,12 @@ export class EventsDdbStack extends cdk.Stack {
         type: dynamodb.AttributeType.STRING,
       },
       timeToLiveAttribute: "ttl",
-      billingMode: dynamodb.BillingMode.PROVISIONED,
-      readCapacity: 1,
-      writeCapacity: 1,
+      billingMode: dynamodb.BillingMode.PAY_PER_REQUEST,
+      // readCapacity: 1,
+      // writeCapacity: 1,
     });
 
+    /**
     const readScale = this.table.autoScaleReadCapacity({
       maxCapacity: 2,
       minCapacity: 1,
@@ -50,5 +51,6 @@ export class EventsDdbStack extends cdk.Stack {
       scaleInCooldown: cdk.Duration.seconds(60),
       scaleOutCooldown: cdk.Duration.seconds(60),
     });
+     */
   }
 }
